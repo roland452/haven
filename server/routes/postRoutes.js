4e56d5fb66326cd1b8481f43c133bfa779b7efc4
roland aminu <aminuroland452@gmail.com>
2026-08-14 02:23:16 +0100

    g

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -63,7 +63,7 @@ router.get('/api/:id', async (req, res) => {
 })
 
 // POST /api/posts
-router.post('/api/posts', userAuth, async (req, res) => {
+router.post('/posts', userAuth, async (req, res) => {
   
   try {
     const {
@@ -110,7 +110,7 @@ router.post('/api/posts', userAuth, async (req, res) => {
 })
 
 // POST /api/posts/:id/donate
-router.post('/api/posts/:id/donate', userAuth, async (req, res) => {
+router.post('/posts/:id/donate', userAuth, async (req, res) => {
   try {
     const { amount } = req.body
     if (!amount || amount <= 0) {
